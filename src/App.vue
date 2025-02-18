b1950f9e7524a1b10016ee31ba5efab220ad9c21
soldierbai <soldierbai@tencent.com>
2025-02-18 19:14:48 +0800

    仓库初始化

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <!-- <RouterView></RouterView> -->
+    <Login/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Login from './components/Login.vue'
+  export default {
+    name: "App",
+    components:{Login}
+  }
+
+</script>
+
+<style>
+  .app {
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center;     /* 垂直居中 */
+  height: 100vh;           /* 使容器占满视口高度 */
+  }
+</style>
